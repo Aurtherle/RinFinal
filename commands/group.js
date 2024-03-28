66da5b88ec0bc74cd97afc22046d17d64b9df496
Aurtherle <113518431+Aurtherle@users.noreply.github.com>
2024-03-28 22:43:57 +0300

    Update group.js

--- commands/group.js
+++ commands/group.js
@@ -35,6 +35,39 @@ cmd({
     )
     //---------------------------------------------------------------------------
 cmd({
+        pattern: "منشن",
+        desc: "Tags every person of group.",
+        category: "group",
+        filename: __filename,
+    },
+    async(Void, citel, text,{ isCreator }) => {
+        if (!citel.isGroup) return citel.reply(tlang().group);
+        const groupMetadata = citel.isGroup ? await Void.groupMetadata(citel.chat).catch((e) => {}) : "";
+        const participants = citel.isGroup ? await groupMetadata.participants : "";
+        const groupAdmins = await getAdmin(Void, citel)
+        const isAdmins = citel.isGroup ? groupAdmins.includes(citel.sender) : false;
+        if (!isAdmins) return citel.reply(tlang().admin);
+
+        let textt = `
+══✪〘   *منشن جماعي*   〙✪══
+
+➲ *الرسالة :* ${text ? text : "blank"}\n\n
+➲ *المشرف:* ${citel.pushName} 🔖
+`
+        for (let mem of participants) {
+            textt += `📍 @${mem.id.split("@")[0]}\n`;
+        }
+        Void.sendMessage(citel.chat, {
+            text: textt,
+            mentions: participants.map((a) => a.id),
+        }, {
+            quoted: citel,
+        });
+    }
+)
+
+//---------------------------------------------------------------------------
+cmd({
             pattern: "ملصق",
             alias: ["s"],
             desc: "Makes sticker of replied image/video.",
